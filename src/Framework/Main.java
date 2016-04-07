faedf7b55e1677ae96a2df3e0ea949136d383e43
javiersb15 <javiersanzbeneito@gmail.com>
2016-04-07 15:06:28 +0200

    afegir main

---
+++ src/Framework/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Framework;
+
+import Framework.Modules.Menu.View.choose_frame;
+import Framework.Modules.Config.Model.Class_config;
+import Framework.Modules.Menu.Controller.Controller_menu;
+import javax.swing.JFrame;
+
+
+/**
+ *
+ * @author usuario
+ */
+public class Main {
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        Class_config.getInstance();
+
+        /* Create and display the form */
+       java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                JFrame choose_frame;
+                new Controller_menu(new choose_frame()).begin();      
+            }
+        });
+    }
+}
